66f2467b1be650beac44971ba3cd95522e74f07b
rbeck20 <remi.beck1@etu.univ-lorraine.fr>
2022-01-28 14:38:18 +0100

    écriture de la fontion mergeSortedArrays

--- src/mergeSortedArrays.js
+++ src/mergeSortedArrays.js
@@ -1,3 +1,33 @@
 function mergeSortedArrays(t1, t2){
-    
+    result = [];
+    i = 0;
+    j = 0;
+    k = 0;
+    while (i < length(t1) && j < length(t2)){
+        if (t1[i] < t2[j]){
+            result[k] = t1[i];
+            i++;
+        }
+        else {
+            result[k] = t2[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < length(t1)){
+        result[k] = t1[i];
+        i++;
+        k++;
+    }
+
+    while (j < length(t2)){
+        result[k] = t2[j];
+        j++;
+        k++;
+    }
+
+    return result;
 }
+
+module.exports = mergeSortedArrays
