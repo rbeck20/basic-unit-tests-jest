b35c70eadb7d3b23ba8bda8dfad6670a6d19fd42
rbeck20 <remi.beck1@etu.univ-lorraine.fr>
2022-01-28 14:59:06 +0100

    écriture de tests pour la fontionc mergeSortedArrays

--- test/mergeSortedArrays.test.js
+++ test/mergeSortedArrays.test.js
@@ -3,5 +3,36 @@ const mergeSortedArrays = require("../src/mergeSortedArrays")
 test('tableaux bien triés', () => {
     t1 = [1, 3, 5];
     t2 = [2, 4];
-    expect(mergeSortedArrays(t1, t2)).toBe([1, 2, 3, 4, 5]);
+    expect(mergeSortedArrays(t1, t2)).toStrictEqual([1, 2, 3, 4, 5]);
+})
+
+test('longueur de tableaux correcte', () => {
+    t1 = [1, 3, 5];
+    t2 = [2, 4];
+    expect(mergeSortedArrays(t1, t2).length).toBe(5);
+})
+
+test('première valeur correcte', () => {
+    t1 = [1, 3, 5];
+    t2 = [2, 4];
+    expect(mergeSortedArrays(t1, t2)[0]).toBe(1);
+})
+
+test('dernière valeur correcte', () => {
+    t1 = [1, 3, 5];
+    t2 = [2, 4];
+    expect(mergeSortedArrays(t1, t2)[mergeSortedArrays(t1, t2).length-1]).toBe(5);
+})
+
+test('première valeur inférieure à la deuxième', () => {
+    t1 = [1, 3, 5];
+    t2 = [2, 4];
+    expect(mergeSortedArrays(t1, t2)[0] < mergeSortedArrays(t1, t2)[1]).toBe(true);
+})
+
+test('dernière valeur supérieure à l avant-dernière', () => {
+    t1 = [1, 3, 5];
+    t2 = [2, 4];
+    expect(mergeSortedArrays(t1, t2)[mergeSortedArrays(t1, t2).length-1]
+        > mergeSortedArrays(t1, t2)[mergeSortedArrays(t1, t2).length-2]).toBe(true);
 })
